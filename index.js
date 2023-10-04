dbc710ced2d4f615c9ac2ce30a4760aea3398434
Gino Guerra <flawlesswf@Ginos-MacBook-Air.local>
2023-10-04 16:24:52 -0400

    created some javascript functions

---
+++ index.js
@@ -0,0 +1,25 @@
+function hello(firstname, secondname) {
+    console.log("hello " + firstname + secondname)  //do something here
+    // const firstname = "isaiah"
+
+}
+// hello("isaiah", "donovan")  
+
+
+function add(num1, num2){
+return(num1 + num2)
+}
+
+
+let sum = add(7,9)
+
+function multiply(num1, num2){
+    return(num1 * num2)
+} 
+multiply(12, 5)
+let answer = multiply(12, 5)
+function combine(add, multiply){
+    return(add + multiply)
+
+}
+console.log(combine(sum, answer))
---
+++ temp.js
@@ -0,0 +1,7 @@
+function celsiusToFahrenheit(celsius){
+
+    return(9 / 5 * celsius + 32 ) 
+}
+const sum = celsiusToFahrenheit()
+
+console.log(celsiusToFahrenheit(32) + " °F")
